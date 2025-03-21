828c07cba58a2e838ad90e634f4f967f967124ee
mutiararflnsh <rifliansahmutiara@gmail.com>
2025-03-21 23:26:53 +0700

    feat: class member

--- library-management-java/library/StaffMember.java
+++ library-management-java/library/StaffMember.java
@@ -1,5 +1,24 @@
 package library;
 
-public class StaffMember {
+public class StaffMember extends Member {
+    private String position;
 
+    public StaffMember(int id, String name, String position) {
+        super(id, name);
+        this.position = position;
+    }
+
+    @Override
+    public void borrowBook(Book book) {
+        if (book.borrowBook()) {
+            borrowedBooks.add(book);
+            System.out.println(name + " (Staff) borrowed \"" + book.getTitle() + "\"");
+        }
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Staff ID: " + id + ", Name: " + name + ", Position: " + position +
+                ", Borrowed Books: " + borrowedBooks.size());
+    }
 }
